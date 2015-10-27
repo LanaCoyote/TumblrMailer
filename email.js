dc75d145921b91d9ea354927c0976787d4eaf723
David Reeve <nerdboy6@gmail.com>
2015-10-27 13:37:26 -0400

    Created email module and added cfg and template files

---
+++ email.js
@@ -0,0 +1,78 @@
+var cfg = require( './cfg.js' ); // separate config info
+var ejs = require( 'ejs' ); // embedded javascript template engine
+var fs  = require( 'fs' ); // filesystem module
+var mandrill = require( 'mandrill-api/mandrill' ); // mandrill email api
+
+var mandrill_client = new mandrill.Mandrill( cfg.MANDRILL_API_KEY );
+
+// Email object
+// Manages storing and sending e-mail messages
+function Email( subject, message, tags ) {
+    this.subject = subject;
+    this.message = message;
+    this.tags    = tags;
+};
+
+// Email.prototype.send
+// Sends the email from the given information to the given information
+Email.prototype.send = function( to_name, to_email, from_name, from_email ) {
+    var msg = {
+        "html":         this.message,
+        "subject":      this.subject,
+        "from_email":   from_email,
+        "from_name":    from_name,
+        "to": [ {
+            "email":    to_email,
+            "name":     to_name
+            } ],
+        "important":    false,
+        "track_opens":  true,
+        "auto_html":    false,
+        "preserve_recipients": true,
+        "merge":        false,
+        "tags":         this.tags
+    };
+    var async = false;
+    var ip_pool = "Main Pool";
+
+    mandrill_client.messages.send(
+        { "message": msg, "async": async, "ip_pool": ip_pool },
+        function ( result ) {
+            console.log( "Mandrill API sent the following message:" );
+            console.log( msg );
+            console.log( "Mandrill API returned the following result: " );
+            console.log( result );
+        }, function ( e ) {
+            console.error( "Mandrill API encountered an error: " + e.name );
+            console.error( "    " + e.message );
+        } );
+}
+
+// EmailTemplate object
+// Convenience object for creating EJS template emails
+function EmailTemplate( template, data ) {
+    this.temp = openEmailTemplate( template );
+    this.data = data;
+}
+
+// EmailTemplate.prototype.render
+// Renders an EJS template using the given template and data
+EmailTemplate.prototype.render = function() {
+    return ejs.render( this.temp, this.data );
+}
+
+// emailFromTemplate
+// Convenience function that creates an Email object from an EJS template
+function emailFromTemplate( subject, template, data, tags ) {
+    var template = new EmailTemplate( template, data );
+    return new Email( subject, template.render(), tags );
+}
+
+function openEmailTemplate( filename ) {
+    return fs.readFileSync( filename, "utf8" );
+}
+
+// module exports
+module.exports = Email;
+module.exports.fromTemplate = emailFromTemplate;
+module.exports.template = EmailTemplate;
